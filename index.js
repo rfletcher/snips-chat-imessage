582ddb3ce007d000734b7904dc37cf5eb28ce109
Rick Fletcher <fletch@pobox.com>
2019-05-11 09:22:32 -0400

    Change initialization order

--- index.js
+++ index.js
@@ -7,28 +7,27 @@ import imessage from "osa-imessage";
 import Snips    from "../snips-chat/lib/snips.js";
 
 const logger = pino({ level: "debug" });
-
-const snips_config = merge.recursive(true, {
-  logger: logger
-}, config.get("snips_chat"));
-
-const snips = new Snips(snips_config);
+const snips  = new Snips(config.get("snips_chat"));
 
 snips.on("text", function(who, what) {
   logger.info(`Send to ${who}: ${what}`);
   imessage.send(who, what);
 });
 
-imessage.listen().on( "message", (msg) => {
-  if (msg.fromMe) {
-    logger.debug(`ignoring message from myself: ${msg.text}`);
-    return;
-  }
+snips.on("connect", function() {
+  imessage.listen().on( "message", (msg) => {
+    if (msg.fromMe) {
+      logger.debug(`ignoring message from myself: ${msg.text}`);
+      return;
+    }
 
-  if (!config.get("allowed_senders").includes(msg.handle)) {
-    logger.warn(`ignoring message from unknown sender: ${msg.handle}: ${msg.text}`);
-    return;
-  }
+    if (!config.get("allowed_senders").includes(msg.handle)) {
+      logger.warn(`ignoring message from unknown sender: ${msg.handle}: ${msg.text}`);
+      return;
+    }
 
-  snips.push(msg.handle, msg.text);
-} );
+    snips.push(msg.handle, msg.text);
+  } );
+
+  logger.info("Waiting for messages");
+});
